7050ba7df1c245cd257ab7d641f25632f1cdba8b
Angela Londoño <l.angela05@gmail.com>
2023-05-25 22:49:19 -0500

    Create componet to Formulario

---
+++ org/src/components/Formulario/Formulario.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import "./Formulario.css"
+import CampoTexto from "../CampoTexto"
+
+const Formulario = () => {
+
+    const [nombre, actualizarNombre] = useState("")
+    const [puesto, actualizarPuesto] = useState("")
+    const [foto, actualizarFoto] = useState("")
+    const [equipo, actualizarEquipo] = useState("")
+
+    const manejarEnvio = (e) => {
+        e.preventDefault()
+        console.log("Manejar el envio")
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto,
+            equipo
+        }
+        console.log(datosAEnviar)
+    }
+
+    return <section className="formulario">
+        <form onSubmit={manejarEnvio}>
+            <h2>Rellena el formulario para crear el colaborador.</h2>
+            <CampoTexto
+                titulo="Nombre"
+                placeholder="Ingresar nombre"
+                required
+                valor={nombre}
+                actualizarValor={actualizarNombre}
+            />
+            <CampoTexto
+                titulo="Puesto"
+                placeholder="Ingresar puesto"
+                required
+                valor={puesto}
+                actualizarValor={actualizarPuesto}
+            />
+            <CampoTexto
+                titulo="Foto"
+                placeholder="Ingresar enlace de foto"
+                required
+                valor={foto}
+                actualizarValor={actualizarFoto}
+            />
+
+        </form>
+    </section>
+}
+
+export default Formulario
